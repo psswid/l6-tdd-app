c0c4f2163ff058786199e23821b97356d47a5e73
Piotr Świderski <swiderski@mda.pl>
2021-04-12 19:01:53 +0200

    my first basic test

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+use App\Project;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::post('/projects', 'ProjectsController@store');
